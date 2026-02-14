30a9747ba727ab4a7ef8a9b5641185b3b00adf66
Omar Ayman <54598439+omarayman98@users.noreply.github.com>
2026-02-14 19:26:11 +0200

    Deploy Flutter Web

--- lib/presentation/features/home/home_screen.dart
+++ lib/presentation/features/home/home_screen.dart
@@ -69,6 +69,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                 ),
                 AboutSection(),
+
                 SkillsSection(),
                 ExperienceSection(),
                 ProjectsSection(key: projectsKey),
